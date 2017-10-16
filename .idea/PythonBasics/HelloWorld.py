fd6a02b0c29b136e394e3591ef981b4a83c65249
Travis <tstigman@gmail.com>
2017-10-16 15:31:47 -0700

    initializedoc

---
+++ .idea/PythonBasics/HelloWorld.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+print(1+2)
+print(7*6)
+print()
+print('The End')
+print("Python's strings are easy to use")
+print ('We can even include " quotes" in strings')
+print ("Hello" + ", World")
+
+greeting = "hello"
+name = "Travis"
+print (greeting + name)
+# if we want a spac, we can add that too ' ' 
+print (greeting + ' ' + name)
